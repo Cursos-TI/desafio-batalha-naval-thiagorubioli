459df3b027b6882cdbd99e7595d3b1cd3dddc78a
Thiago Rubioli <thiagofmc@gmail.com>
2025-09-30 01:31:37 +0000

    Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,59 +1,59 @@
 #include <stdio.h>
 
-int main () {
+#define LINHA 10
+#define COLUNA 10
+
+int main() {
 
     printf("\n>>>>> Jogo de Batalha Naval <<<<<\n\n");
     
-    // Declaração e inicialização do vetor de letras (colunas)
-    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    // Declaração e inicialização do vetor de letras (colunas) do tabuleiro
+    char letras[LINHA];
+    char letra_ini = 'A';
+
+    // Declaração e inicialização da numeração das linhas do tabuleiro
+    int linhas[LINHA];
+    int linha_ini = 1;
     
-    // Declaração e inicialização da matriz de 10x10 (tabuleiro)
-    int tabuleiro[10][11] = {
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    // Declaração da matriz de 10x10 (tabuleiro)
+    int matriz[LINHA][COLUNA];
 
     // Imprimindo a linha de letras (cabeçalho das colunas "A" até "J")
-    printf("  ");
+    printf("   ");
     for (int i = 0; i < 10; i++) {
-        printf(" %c", letras[i]);
+        letras[i] = letra_ini;
+        letra_ini++;
+        printf("%c ", letras[i]);
     }
 
     printf("\n");
-      
-    // Acessando e imprimindo o restante dos elementos da matriz (tabuleiro e navios)
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 11; j++) {
-            if (i == 3 && j > 3 && j < 7) // Adicionando um navio horizontal na linha 4, colunas D, E e F
+
+    // Acessando e imprimindo tabuleiro com os navios posicionados
+    for (int i = 0; i < LINHA; i++) {      // Loop externo para as linhas
+        linhas[i] = linha_ini;
+        linha_ini++;
+        printf("%2d ", linhas[i]);
+        for (int j = 0; j < COLUNA; j++) {  // Loop interno para as colunas
+            if (i == 3 && j > 3 && j < 7) // Adicionando um navio horizontal na linha 4, colunas E, F e G
             {
                 printf("3 "); // Imprimindo o número "3" para representar a localização do navio horizontal
 
-            } else if (j == 8 && i > 4 && i < 8) // Adicionando um navio vertical na coluna H, linhas 6, 7 e 8
+            } else if (j == 8 && i > 4 && i < 8) // Adicionando um navio vertical na coluna I, linhas 6, 7 e 8
             {
                 printf("3 "); // Imprimindo o número "3" para representar a localização do navio vertical
 
+            } else if (i == j && j > 3 && j < 7) // Adicionando um navio diagonal nas linhas 5, 6 e 7, colunas E, F e G
+            {
+                printf("3 "); // Imprimindo o número "3" para representar a localização do navio diagonal
+
             } else
             {
-                if (i < 9 && j == 0)
-                {
-                    printf(" %d ", tabuleiro[i][j]); // Imprimindo o restante do tabuleiro com um espaço adicional antes das linhas 1-9
-                } else
-                {
-                    printf("%d ", tabuleiro[i][j]); // Imprimindo a linha 10 sem espaço adicional para regularizar o tabuleiro
-                }
+            matriz[i][j] = 0;
+            printf("%d ", matriz[i][j]); // Imprimindo o número "0" para representar água
             }
         }
         printf("\n");
     }
-
+ 
     return 0;
-
 }

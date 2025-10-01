d4319ff2f529f9df92aee9edcc7b6cb810776d19
Thiago Rubioli <thiagofmc@gmail.com>
2025-10-01 00:58:22 +0000

    Nível Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 #define LINHA 10
 #define COLUNA 10
 
@@ -7,50 +9,88 @@ int main() {
 
     printf("\n>>>>> Jogo de Batalha Naval <<<<<\n\n");
     
-    // Declaração e inicialização do vetor de letras (colunas) do tabuleiro
-    char letras[LINHA];
-    char letra_ini = 'A';
-
-    // Declaração e inicialização da numeração das linhas do tabuleiro
-    int linhas[LINHA];
-    int linha_ini = 1;
-    
-    // Declaração da matriz de 10x10 (tabuleiro)
     int matriz[LINHA][COLUNA];
 
-    // Imprimindo a linha de letras (cabeçalho das colunas "A" até "J")
-    printf("   ");
-    for (int i = 0; i < 10; i++) {
-        letras[i] = letra_ini;
-        letra_ini++;
-        printf("%c ", letras[i]);
+    // Inicializar toda a matriz com água (0)
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            matriz[i][j] = 0;
+        }
     }
 
+    // Posicionar navio horizontal
+    for (int j = 4; j <= 6; j++) {
+        matriz[3][j] = 3;
+    }
+
+    // Posicionar navio vertical
+    for (int i = 0; i <= 2; i++) {
+        matriz[i][8] = 3;
+    }
+
+    // Posicionar navio diagonal
+    for (int i = 5; i <= 7; i++) {
+        matriz[i][i - 3] = 3;
+    }
+
+    // Posicionar cruz
+    for (int i = 0; i <= 4; i++) { // Eixo vertical
+        matriz[i][2] = 5;
+        for (int j = 0; j <= 4; j++) { // Eixo horizontal
+        matriz[2][j] = 5;
+        }
+    }
+
+    // Posicionar cone
+    for (int i = 6; i <= 8; i++) {
+        matriz[i][2] = 5;
+        for (int j = 0; j <= 4; j++) {
+            if (j < 4 && j > 0 && i == 7) {
+                matriz[i][j] = 5;
+            } else if (j < 5 && j >= 0 && i == 8) {
+                matriz[i][j] = 5;
+            }
+        }
+    }
+    
+    // Posicionar octaedro (linhas 6-10, centro coluna H)
+    int centro_octaedro = 7;  // Coluna H (índice 7)
+    int linha_inicial_octaedro = 5;  // Linha 6 (índice 5)
+
+    for (int i = 0; i <= 4; i++) {  // 5 linhas do octaedro (0 a 4)
+        int linha_atual = linha_inicial_octaedro + i;
+        
+        // Calcular largura: cresce até o meio, depois diminui
+        int largura;
+        if (i <= 2) {
+            // Fase de expansão: 1, 3, 5
+            largura = 1 + (i * 2);
+        } else {
+            // Fase de contração: 3, 1
+            largura = 1 + ((4 - i) * 2);
+        }
+        
+        // Calcular deslocamento do centro
+        int deslocamento = (largura - 1) / 2;
+        
+        // Preencher a linha
+        for (int j = centro_octaedro - deslocamento; j <= centro_octaedro + deslocamento; j++) {
+            matriz[linha_atual][j] = 5;
+        }
+    }
+
+    // Imprimir cabeçalho com letras
+    printf("   ");
+    for (int i = 0; i < COLUNA; i++) {
+        printf("%c ", 'A' + i);
+    }
     printf("\n");
 
-    // Acessando e imprimindo tabuleiro com os navios posicionados
-    for (int i = 0; i < LINHA; i++) {      // Loop externo para as linhas
-        linhas[i] = linha_ini;
-        linha_ini++;
-        printf("%2d ", linhas[i]);
-        for (int j = 0; j < COLUNA; j++) {  // Loop interno para as colunas
-            if (i == 3 && j > 3 && j < 7) // Adicionando um navio horizontal na linha 4, colunas E, F e G
-            {
-                printf("3 "); // Imprimindo o número "3" para representar a localização do navio horizontal
-
-            } else if (j == 8 && i > 4 && i < 8) // Adicionando um navio vertical na coluna I, linhas 6, 7 e 8
-            {
-                printf("3 "); // Imprimindo o número "3" para representar a localização do navio vertical
-
-            } else if (i == j && j > 3 && j < 7) // Adicionando um navio diagonal nas linhas 5, 6 e 7, colunas E, F e G
-            {
-                printf("3 "); // Imprimindo o número "3" para representar a localização do navio diagonal
-
-            } else
-            {
-            matriz[i][j] = 0;
-            printf("%d ", matriz[i][j]); // Imprimindo o número "0" para representar água
-            }
+    // Imprimir tabuleiro
+    for (int i = 0; i < LINHA; i++) {
+        printf("%2d ", i + 1);
+        for (int j = 0; j < COLUNA; j++) {
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
